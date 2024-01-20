69c8ea37f6bd1e6afb4d8b017c9fdbc5bcad5ba4
xiaohuqwq <2454473536@qq.com>
2024-01-20 23:03:11 +0800

    2021 01 20 23:30

--- miniprogram/pages/detailPage/detailPage.js
+++ miniprogram/pages/detailPage/detailPage.js
@@ -1,27 +1,49 @@
 // pages/detailPage/detailPage.js
-Page({
+
+//读取和更新数据库还没写，页面上显示的是本地测试
+
+Page({ 
 
   /**
    * 页面的初始数据
    */
   data: {
-    id:"111"
+    id:"99",
+    author:"null",
+    category:"null",
+    time:null,
+    detail:["陷入爱河的人们仿佛既有了盔甲，也有了软肋。在神经生物学家眼中，这种复杂的情感能够被大脑中多巴胺、催产素等神经递质的...",
+    "这项研究于2024年1月12日发表在《细胞》（Cell）子刊《当代生物学》（Current Biology）上，作...",
+    "多巴胺（dopamine）是一种由神经元释放并用以传递信息的重要神经递质，与包括大脑奖励系统在内的多个神经通路有关...",
+    "“作为人类，对于不同的人，无论是浪漫伴侣还是亲密朋友，我们与之互动的渴望程度都有所区别和选择。这定义了我们的整个社..."],
+    title:"《细胞》子刊：问世间情为何物？神经生物学家：多巴胺",
+    isCollect:true,
+    isLike:true,
+    path:"cloud://information-1ggxsb6kb9c06827.696e-information-1ggxsb6kb9c06827-1323855695/news01.jpg",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-    this.setData({
-      id:options.news_id
-    })
+  onLoad() {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
+  onReady(options) {
+    this.setData({
+      id:options.id,
+      author:options.author,
+      category:options.category,
+      detail:options.detail,
+      title:options.title,
+      path:options.path,
+      isCollect:options.isCollect,
+      isLike:options.isLike,
+      time:options.time,
+    })
   },
 
   /**
@@ -64,5 +86,27 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+
+  goBack()
+  {
+    wx.navigateBack({delta:0,})
+  },
+
+  pressLike()
+  {
+    this.setData({
+      isLike:!this.data.isLike,
+    })
+    console.log(this.data)
+  },
+
+  pressCollect()
+  {
+    this.setData({
+      isCollect:!this.data.isCollect,
+    })
+    console.log(this.data)
+  },
+
 })
